2e2186af116d06c86197d39c6415a5dcd3a6f743
Peter Armstrong <peterarmstrong999@gmail.com>
2021-08-07 09:46:08 +0100

    Update script.js

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,15 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(items) {
+  let contentList = document.querySelector("#content")
+
+  items.forEach((item) => {
+    let unordered = document.createElement("ul");
+    contentList.appendChild(unordered);
+
+    let list = document.createElement("li");
+    unordered.appendChild(list);
+    list.innerText = item;
+  });
+  
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
