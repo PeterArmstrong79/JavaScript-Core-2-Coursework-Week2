6747c70f33ed0b00e3e78c2bedc372a32b70ac00
Peter Armstrong <peterarmstrong999@gmail.com>
2021-08-07 09:45:24 +0100

    1st 2 exercises

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,15 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+
+arrayOfPeople.forEach((person) => {
+  let name = document.createElement("h1");
+  content.appendChild(name);
+  name.innerText = "my name is " + person.name;
+
+  let job = document.createElement("h2");
+  content.appendChild(job);
+  job.innerText = "my job is " + person.job;
+});
 }
 
 let people = [
